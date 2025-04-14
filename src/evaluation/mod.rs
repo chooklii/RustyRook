9f7e8e38af500db4a8f45874d1a091824c2f11e1
Benjamin Zenth <benjaminzenth@icloud.com>
2025-04-14 17:47:53 +0200

    add King Position Evaluation and Push Opponent King to the Edge

--- src/evaluation/mod.rs
+++ src/evaluation/mod.rs
@@ -1,6 +1,8 @@
+use std::usize;
+
 use rustc_hash::FxHashMap;
 
-use crate::{board::board::Chessboard, figures::{bishop::{self, Bishop}, figures::Figure, queen::{self, Queen}, rook::Rook}, helper::moves_by_field::MoveInEveryDirection};
+use crate::{board::board::Chessboard, figures::{bishop::Bishop, figures::Figure, queen::Queen, rook::Rook}, helper::moves_by_field::MoveInEveryDirection};
 
 #[derive(PartialEq, PartialOrd, Clone, Debug, Copy)]
 pub struct Evaluation {
@@ -20,7 +22,7 @@ impl Default for Evaluation {
 }
 
 // a1 to h8
-const PAWN_RATE: [f32; 64] = [
+const PAWN_RATE_KING_CENTER: [f32; 64] = [
     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
     0.8, 0.8, 1.0, 1.1, 1.1, 1.0, 0.8, 0.8, 
@@ -28,6 +30,28 @@ const PAWN_RATE: [f32; 64] = [
     1.0, 1.0, 1.2, 1.3, 1.3, 1.2, 1.0, 1.0, 
     1.0, 1.0, 1.1, 1.1, 1.1, 1.1, 1.0, 1.0, 
     2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 
+    9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 
+];
+
+const PAWN_RATE_KING_LEFT: [f32; 64] = [
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+    0.7, 0.7, 0.7, 1.1, 1.1, 1.1, 1.1, 1.1, 
+    0.7, 0.7, 0.7, 1.3, 1.3, 1.3, 1.3, 1.3, 
+    1.0, 1.0, 1.2, 1.3, 1.3, 1.3, 1.3, 1.3, 
+    1.0, 1.0, 1.1, 1.1, 1.1, 1.3, 1.3, 1.3, 
+    2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.5, 2.5, 
+    9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 
+];
+
+const PAWN_RATE_KING_RIGHT: [f32; 64] = [
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+    1.1, 1.1, 1.1, 1.1, 1.1, 0.7, 0.7, 0.7, 
+    1.3, 1.3, 1.3, 1.3, 1.3, 1.7, 0.7, 0.7, 
+    1.3, 1.3, 1.3, 1.3, 1.3, 1.2, 1.0, 1.0, 
+    1.3, 1.3, 1.3, 1.1, 1.1, 1.1, 1.0, 1.0, 
+    2.5, 2.5, 2.5, 2.0, 2.0, 2.0, 2.0, 2.0, 
     9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 
 ];
 
@@ -97,6 +121,35 @@ const LATE_GAME_KING_RATE: [f32; 64] = [
     1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 
 ];
 
+enum KingPosition{
+    CENTER,
+    LEFT,
+    RIGHT
+}
+
+fn get_king_position(figures: &FxHashMap<usize, Figure>) -> usize{
+    let (&king_position, _) = figures.iter()
+                                            .find(|(_, fig)| fig.is_king())
+                                            // if we have no king we have other problems :-D
+                                            .unwrap();
+    return king_position;
+}
+
+fn check_where_king_is_located(king_position: &usize, is_black_king: bool) -> KingPosition{
+    if is_black_king{
+        return match king_position % 8{
+            0 | 1 | 2 => KingPosition::RIGHT,
+            5 | 6 | 7 => KingPosition::LEFT,
+            _ => KingPosition::CENTER
+        }
+    }                                    
+    match king_position % 8{
+        0 | 1 | 2 => KingPosition::LEFT,
+        5 | 6 | 7 => KingPosition::RIGHT,
+        _ => KingPosition::CENTER
+    }
+}
+
 fn get_king_weight(position: usize, pieces: f32) -> f32 {
     // if there is less then 10 pieces value left activate king
     if pieces >=10.0{
@@ -105,27 +158,36 @@ fn get_king_weight(position: usize, pieces: f32) -> f32 {
     return LATE_GAME_KING_RATE[position];
 }
 
+fn get_pawn_rate(position: usize, king_position: &KingPosition) -> f32{
+    // push pawns on side where out king is not!
+    match king_position{
+        KingPosition::CENTER => PAWN_RATE_KING_CENTER[position],
+        KingPosition::LEFT => PAWN_RATE_KING_LEFT[position],
+        KingPosition::RIGHT => PAWN_RATE_KING_RIGHT[position]
+    }
+}
+
 fn get_rook_weight(rook: &Rook, position: usize, board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> f32{
     let threatened_fields = rook.threatened_fields(&board, &position, &moves_by_field, &64);
-    let threat_bonus = threatened_fields.len() as f32 * 0.05;
+    let threat_bonus = threatened_fields.len() as f32 * 0.02;
     return ROOK_RATE[position] + threat_bonus;
 }
 
 fn get_bishop_weight(bishop: &Bishop, position: usize, board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> f32{
     let threatened_fields = bishop.threatened_fields(&board, &position, &moves_by_field, &64);
-    let threat_bonus = threatened_fields.len() as f32 * 0.05;
+    let threat_bonus = threatened_fields.len() as f32 * 0.02;
     return BISHOP_RATE[position] + threat_bonus;
 }
 
 fn get_queen_weight(queen: &Queen, position: usize, board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> f32{
     let threatened_fields = queen.threatened_fields(&board, &position, &moves_by_field, &64);
-    let threat_bonus = threatened_fields.len() as f32 * 0.05;
+    let threat_bonus = threatened_fields.len() as f32 * 0.02;
     return QUEEN_RATE[position] + threat_bonus;
 }
 
-fn get_figure_position_weight(figure: &Figure, position: usize, board: &Chessboard, pieces: f32, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> f32 {
+fn get_figure_position_weight(figure: &Figure, position: usize, board: &Chessboard, king_position: &KingPosition, pieces: f32, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> f32 {
     return match figure {
-        Figure::Pawn(_) => PAWN_RATE[position],
+        Figure::Pawn(_) => get_pawn_rate(position, &king_position),
         Figure::Knight(_) => KNIGHT_RATE[position],
         Figure::Bishop(bishop) => get_bishop_weight(&bishop, position, &board, &moves_by_field),
         Figure::Rook(rook) => get_rook_weight(&rook, position, &board, &moves_by_field),
@@ -145,6 +207,20 @@ fn get_figure_weight(figure: &Figure) -> f32 {
     };
 }
 
+fn get_opponent_king_bonus(piece_value: f32, king_position: &usize) -> f32{
+    if piece_value > 15.0{
+        return 0.0
+    }
+    let king_column = king_position % 8 ;
+    if king_position > &55 || king_position < &8 || king_column == 0 || king_column == 7{
+        return 2.0;
+    };
+    if king_position > &47 || king_position < &15 || king_column == 1 || king_column == 6{
+        return 1.0;
+    }
+    return 0.0;
+}
+
 pub fn evaluate(board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEveryDirection>) -> Evaluation {
     let white_pieces_value: f32 = board
         .white_figures
@@ -152,10 +228,16 @@ pub fn evaluate(board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEver
         .map(|(_, figure)| get_figure_weight(figure))
         .sum();
 
+    let white_king_usize =  get_king_position(&board.white_figures);
+    let black_king_usize = get_king_position(&board.black_figures);
+
+    let white_king_position = check_where_king_is_located(&white_king_usize, false);
+    let black_king_position = check_where_king_is_located(&black_king_usize, true);
+
     let white_pieces_position_value: f32 = board
         .white_figures
         .iter()
-        .map(|(&position, figure)| get_figure_position_weight(&figure, position, &board, white_pieces_value, &moves_by_field))
+        .map(|(&position, figure)| get_figure_position_weight(&figure, position, &board, &white_king_position, white_pieces_value, &moves_by_field))
         .sum();
 
     let black_pieces_value: f32 = board
@@ -168,13 +250,18 @@ pub fn evaluate(board: &Chessboard, moves_by_field: &FxHashMap<usize, MoveInEver
     let black_pieces_position_value: f32 = board
         .black_figures
         .iter()
-        .map(|(&position, figure)| get_figure_position_weight(&figure, 63 - position, &board, black_pieces_value, &moves_by_field))
+        .map(|(&position, figure)| get_figure_position_weight(&figure, 63 - position,&board, &black_king_position, black_pieces_value, &moves_by_field))
         // invert black position to use same values for both colors
         .sum();
 
 
-    let white_value = white_pieces_value + white_pieces_position_value;
-    let black_value = black_pieces_value + black_pieces_position_value;
+    // in the endgame push opponent king to the edge of the board
+    let white_opponent_king_bonus = get_opponent_king_bonus(black_pieces_value, &black_king_usize);
+    let black_opponent_king_bonus = get_opponent_king_bonus(white_pieces_value, &white_king_usize);
+
+
+    let white_value = white_pieces_value + white_pieces_position_value + white_opponent_king_bonus;
+    let black_value = black_pieces_value + black_pieces_position_value + black_opponent_king_bonus;
 
     Evaluation {
         white_pieces_value: white_value,
